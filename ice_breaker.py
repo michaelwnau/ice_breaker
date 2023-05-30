3cc10654a71f0c4781b7ff8bba77af4ffd90d550
Michael Nau <michaelwnau@gmail.com>
2023-05-30 18:55:21 -0500

    add LangChain PromptTemplate

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,2 +1,11 @@
+from langchain import PromptTemplate
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import LLMChain
+
+
+
+
+
+
 if __name__ ==  '__main__':
     print("Hello LangChain!")
