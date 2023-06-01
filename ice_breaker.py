18cb7963cc9bb77980bebf0039a542e788b6cabc
Michael Nau <michaelwnau@gmail.com>
2023-05-31 22:09:40 -0500

    update libraries

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,3 +1,4 @@
+import langchain as lc
 from langchain import PromptTemplate
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
--- third_parties/linkedin.py
+++ third_parties/linkedin.py
@@ -5,5 +5,6 @@ def scrape_linkedin_profile(linkedin_profile_url: str):
     """scrape information from linkedin profiles, 
     manually scrape the information from the LinkedIn profile page
     """
-    pass
+    api_endpoint = "https://nubela.co/proxycurl/api/v2/linkedin"
+    header_dic + {"Authorization": f'Bearer {os.environ.get("PROXYCURL_API_KEY")}'}
 
